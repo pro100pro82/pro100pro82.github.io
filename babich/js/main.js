138b67c8128d4fac838bdcb41c1be5478af06bf3
pro100pro82 <74148896+pro100pro82@users.noreply.github.com>
2021-05-02 16:19:33 +0300

    02.05/3

--- babich/js/main.js
+++ babich/js/main.js
@@ -3,8 +3,17 @@ document.addEventListener("DOMContentLoaded", function(){
 
   body.classList.add("is-load");
 })
-$(document).ready(function() {
-  $('.header__burger').click(function() {
-    $('.header__burger,.nav').toggleClass('active');
+
+document.addEventListener('DOMContentLoaded', function() {
+  var open = document.querySelector(".js-menu-open");
+  var close = document.querySelector(".js-menu-close");
+  var menu = document.querySelector(".js-menu");
+
+
+  open.addEventListener("click", function() {
+    menu.classList.add("is-open");
   })
-})
+  close.addEventListener("click", function() {
+    menu.classList.remove("is-open");
+  })
+});
